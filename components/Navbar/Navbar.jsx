3ce3b922ebe9b992af0c36383293f946b8502dbb
sohaila277 <sohailamohameddd@gmail.com>
2026-03-03 22:03:37 +0200

    edited the responsivness of the about and expert section components, as well as the footer social media icons. Also made minor adjustments to the navbar for better mobile experience.

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../src/assets/images/logo.png";
-import { FiSearch, FiUser, FiShoppingBag } from "react-icons/fi";
+import { FiSearch, FiUser, FiShoppingBag, FiMenu, FiX } from "react-icons/fi";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <nav className="navbar">
       <div className="logo">
-        <img src={logo} alt="Glass Skin Effect Logo" />
+        <img src={logo} alt="Logo" />
       </div>
 
-      <ul className="menu">
-        <li><a href="#hero">HOME</a></li>
-        <li><a href="#about">ABOUT</a></li>
-        <li><a href="#services">SKIN CARE</a></li>
-        <li><a href="#expert">BODY CARE</a></li>
-        <li><a href="#professional">TREATMENTS</a></li>
-        <li><a href="#products">PRODUCTS</a></li>
-        <li><a href="#footer">CONTACT</a></li>
+      <ul className={isOpen ? "menu active" : "menu"}>
+        <li><a href="#hero" onClick={toggleMenu}>HOME</a></li>
+        <li><a href="#about" onClick={toggleMenu}>ABOUT</a></li>
+        <li><a href="#services" onClick={toggleMenu}>SKIN CARE</a></li>
+        <li><a href="#expert" onClick={toggleMenu}>BODY CARE</a></li>
+        <li><a href="#professional" onClick={toggleMenu}>TREATMENTS</a></li>
+        <li><a href="#products" onClick={toggleMenu}>PRODUCTS</a></li>
+        <li><a href="#footer" onClick={toggleMenu}>CONTACT</a></li>
       </ul>
 
-      <div className="icons">
-        <FiSearch className="nav-icon" />
-        <FiUser className="nav-icon" />
-        <FiShoppingBag className="nav-icon" />
+      <div className="nav-right">
+        <div className="icons">
+          <FiSearch className="nav-icon" />
+          <FiUser className="nav-icon" />
+          <FiShoppingBag className="nav-icon" />
+        </div>
+
+        {/* Hamburger Toggle Button */}
+        <div className="hamburger" onClick={toggleMenu}>
+          {isOpen ? <FiX /> : <FiMenu />}
+        </div>
       </div>
     </nav>
   );
